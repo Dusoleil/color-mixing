987c6674a61c5c91485c66e11f58ee07725f7905
dusoleil <howcansocksbereal@gmail.com>
2025-07-14 13:35:09 -0400

    prototype drawing color swatches

--- index.js
+++ index.js
@@ -15,4 +15,21 @@ pick_targets.onchange = e =>
     }
 }
 
+var swatches = document.getElementById("swatches");
+add_swatch(swatches, "#FF0000");
+add_swatch(swatches, "#00FF00");
+add_swatch(swatches, "#0000FF");
+
+}
+
+function add_swatch(parent, color)
+{
+    var swatch = document.createElement("canvas");
+    swatch.classList.add("swatch");
+    const ctx = swatch.getContext("2d");
+
+    ctx.fillStyle = color;
+    ctx.fillRect(0,0,swatch.width,swatch.height);
+
+    parent.appendChild(swatch);
 }
