72053de6d68a877fe6ec3c066d137f753275f96b
dusoleil <howcansocksbereal@gmail.com>
2025-08-09 09:44:04 -0400

    Restructure the validity tab to remove the redundant or unrelated info and give the rest better UX

--- validity_check.js
+++ validity_check.js
@@ -7,10 +7,6 @@ import {vec3} from "glMatrix"
 
 export var validity_check =
 {
-    data()
-    {return{
-        old_sp:[0,0,0,0]
-    }},
     created()
     {
         this.DETAIL_LEVEL = DETAIL_LEVEL;
@@ -52,19 +48,6 @@ export var validity_check =
         current_barycentric()
         {
             return this.barycentric_coords(this.current,this.comp_colors);
-        },
-        new_sp()
-        {
-            if(this.comp_colors.length < 2)
-                return [0,0,0,0];
-            return proj.calculate_setpoints(this.old_sp,this.current_barycentric,this.target_barycentric);
-        }
-    },
-    watch:
-    {
-        comp_colors()
-        {
-            this.old_sp.fill(0);
         }
     },
     methods:
@@ -83,7 +66,7 @@ export var validity_check =
         closest_to_hull(p,h)
         {
             if(h.length == 1)
-                return proj.point_to_point_distance(p.XYZ,h[0].XYZ);
+                return h[0];
             if(h.length == 2)
             {
                 let projection = proj.project_onto_line_segment(p.XYZ,h[0].XYZ,h[1].XYZ);
@@ -120,70 +103,39 @@ export var validity_check =
     template:/*html*/`
         <div class="mx-auto mt-10 mb-4" :style="{'max-width':'85dvw'}">
             <plot v-if="comp_colors.length >= 1"></plot>
-            <v-card title="Closest Possible Colors" elevation="10"><v-card-text>
-                <template v-if="comp_colors.length < 1">
-                    <v-label>No Component Colors to Work With</v-label>
-                </template>
-                <template v-if="comp_colors.length == 1">
-                    <v-label>One Component Color</v-label>
-                    <v-divider color="primary"></v-divider>
-                    {{target.name}} is {{target_distance_to_hull.toFixed(4)}} away from {{comp_colors[0].name}}
-                    <v-divider class="border-opacity-0"></v-divider>
-                    {{current.name}} is {{current_distance_to_hull.toFixed(4)}} away from {{comp_colors[0].name}}
-                </template>
-                <template v-if="comp_colors.length == 2">
-                    <v-label>Two Component Colors</v-label>
-                    <v-divider class="border-opacity-0"></v-divider>
-                    {{target.name}} is {{target_distance_to_hull.toFixed(4)}} away from the line.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    {{current.name}} is {{current_distance_to_hull.toFixed(4)}} away from the line.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    The estimated makeup of {{target.name}} is {{target_barycentric[0].toFixed(4)}} {{comp_colors[0].name}} and {{target_barycentric[1].toFixed(4)}} {{comp_colors[1].name}}.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    The estimated makeup of {{current.name}} is {{current_barycentric[0].toFixed(4)}} {{comp_colors[0].name}} and {{current_barycentric[1].toFixed(4)}} {{comp_colors[1].name}}.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    <v-text-field type="number" :style="{'min-width':'12ch'}" :label="comp_colors[1].name+' SP'" v-model.number="old_sp[1]"></v-text-field>
-                    <v-text-field disabled type="number" :style="{'min-width':'12ch'}" label="New SP" v-model.number="new_sp[1]"></v-text-field>
-                    <v-divider class="border-opacity-0"></v-divider>
-                </template>
-                <template v-if="comp_colors.length == 3">
-                    <v-label>Three Component Colors</v-label>
-                    <v-divider class="border-opacity-0"></v-divider>
-                    {{target.name}} is {{target_distance_to_hull.toFixed(4)}} away from the triangle.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    {{current.name}} is {{current_distance_to_hull.toFixed(4)}} away from the triangle.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    The estimated makeup of {{target.name}} is {{target_barycentric[0].toFixed(4)}} {{comp_colors[0].name}}, {{target_barycentric[1].toFixed(4)}} {{comp_colors[1].name}}, and {{target_barycentric[2].toFixed(4)}} {{comp_colors[2].name}}.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    The estimated makeup of {{current.name}} is {{current_barycentric[0].toFixed(4)}} {{comp_colors[0].name}}, {{current_barycentric[1].toFixed(4)}} {{comp_colors[1].name}}, and {{current_barycentric[2].toFixed(4)}} {{comp_colors[2].name}}.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    <v-text-field type="number" :style="{'min-width':'12ch'}" :label="comp_colors[1].name+' SP'" v-model.number="old_sp[1]"></v-text-field>
-                    <v-text-field disabled type="number" :style="{'min-width':'12ch'}" label="New SP" v-model.number="new_sp[1]"></v-text-field>
-                    <v-text-field type="number" :style="{'min-width':'12ch'}" :label="comp_colors[2].name+' SP'" v-model.number="old_sp[2]"></v-text-field>
-                    <v-text-field disabled type="number" :style="{'min-width':'12ch'}" label="New SP" v-model.number="new_sp[2]"></v-text-field>
-                    <v-divider class="border-opacity-0"></v-divider>
-                </template>
-                <template v-if="comp_colors.length >= 4">
-                    <v-label>Four Component Colors</v-label>
-                    <v-divider class="border-opacity-0"></v-divider>
-                    {{target.name}} is {{target_distance_to_hull.toFixed(4)}} away from the convex hull.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    {{current.name}} is {{current_distance_to_hull.toFixed(4)}} away from the convex hull.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    The estimated makeup of {{target.name}} is {{target_barycentric[0].toFixed(4)}} {{comp_colors[0].name}}, {{target_barycentric[1].toFixed(4)}} {{comp_colors[1].name}}, {{target_barycentric[2].toFixed(4)}} {{comp_colors[2].name}}, and {{target_barycentric[3].toFixed(4)}} {{comp_colors[3].name}}.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    The estimated makeup of {{current.name}} is {{current_barycentric[0].toFixed(4)}} {{comp_colors[0].name}}, {{current_barycentric[1].toFixed(4)}} {{comp_colors[1].name}}, {{current_barycentric[2].toFixed(4)}} {{comp_colors[2].name}}, and {{current_barycentric[3].toFixed(4)}} {{comp_colors[3].name}}.
-                    <v-divider class="border-opacity-0"></v-divider>
-                    <v-text-field type="number" :style="{'min-width':'12ch'}" :label="comp_colors[1].name+' SP'" v-model.number="old_sp[1]"></v-text-field>
-                    <v-text-field disabled type="number" :style="{'min-width':'12ch'}" label="New SP" v-model.number="new_sp[1]"></v-text-field>
-                    <v-text-field type="number" :style="{'min-width':'12ch'}" :label="comp_colors[2].name+' SP'" v-model.number="old_sp[2]"></v-text-field>
-                    <v-text-field disabled type="number" :style="{'min-width':'12ch'}" label="New SP" v-model.number="new_sp[2]"></v-text-field>
-                    <v-text-field type="number" :style="{'min-width':'12ch'}" :label="comp_colors[3].name+' SP'" v-model.number="old_sp[3]"></v-text-field>
-                    <v-text-field disabled type="number" :style="{'min-width':'12ch'}" label="New SP" v-model.number="new_sp[3]"></v-text-field>
-                    <v-divider class="border-opacity-0"></v-divider>
-                </template>
-            </v-card-text></v-card>
-            <color-viewer :detail="DETAIL_LEVEL.PARTIAL" v-if="comp_colors.length >= 2" :color="target_closest_to_hull"></color-viewer>
-            <color-viewer :detail="DETAIL_LEVEL.PARTIAL" v-if="comp_colors.length >= 2" :color="current_closest_to_hull"></color-viewer>
+            <v-card elevation="10">
+                <v-card-title>Estimated Makeup</v-card-title>
+                <v-card-subtitle>
+                    <template v-if="comp_colors.length < 1">
+                        No Component Colors to Work With
+                    </template>
+                    <template v-if="comp_colors.length == 1">
+                        One Component Color
+                    </template>
+                    <template v-if="comp_colors.length == 2">
+                        Two Component Colors
+                    </template>
+                    <template v-if="comp_colors.length == 3">
+                        Three Component Colors
+                    </template>
+                    <template v-if="comp_colors.length >= 4">
+                        Four Component Colors
+                    </template>
+                </v-card-subtitle>
+                <v-card-text v-if="comp_colors.length >= 1">
+                    <v-label>Target:</v-label>
+                    <v-progress-linear v-for="(color,idx) in comp_colors" :color="color.hex" v-model="target_barycentric[idx]" max="1" height="25">
+                        {{color.name}} : {{target_barycentric[idx].toFixed(4)}}
+                    </v-progress-linear>
+                    <v-label class="mt-4">Current:</v-label>
+                    <v-progress-linear v-for="(color,idx) in comp_colors" :color="color.hex" v-model="current_barycentric[idx]" max="1" height="25">
+                        {{color.name}} : {{current_barycentric[idx].toFixed(4)}}
+                    </v-progress-linear>
+                </v-card-text>
+            </v-card>
+            <div class="d-flex flex-wrap justify-center mt-8 ga-16">
+                <color-viewer :detail="DETAIL_LEVEL.PARTIAL" v-if="comp_colors.length >= 1" :color="target_closest_to_hull"></color-viewer>
+                <color-viewer :detail="DETAIL_LEVEL.PARTIAL" v-if="comp_colors.length >= 1" :color="current_closest_to_hull"></color-viewer>
+            </div>
         </div>`
 };
